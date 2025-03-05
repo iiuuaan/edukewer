9d16d195871d6d4c0d416d1c3a73cc704ceea739
iuan <setiawanahmadbudi415@gmail.com>
2025-03-05 18:17:53 +0800

    Added dashboard and course detail pages

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,20 @@
 document.addEventListener("DOMContentLoaded", function() {
     const loginForm = document.querySelector(".login-form");
+    const logoutBtn = document.querySelector("#logout");
 
     if (loginForm) {
         loginForm.addEventListener("submit", function(event) {
-            event.preventDefault(); // Mencegah form melakukan submit default
+            event.preventDefault();
             alert("Login successful!");
             window.location.href = "dashboard.html";
+        });
+    }
+
+    if (logoutBtn) {
+        logoutBtn.addEventListener("click", function(event) {
+            event.preventDefault();
+            alert("Logged out successfully!");
+            window.location.href = "index.html";
         });
     }
 });
